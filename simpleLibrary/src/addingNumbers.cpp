ed601347d997c6b63d1b465210ee56fec82044a3
Bert Taeymans <bert.taeymans@tiberium.ch>
2018-01-29 22:16:33 +0100

    Updated addingNumbers

--- simpleLibrary/src/addingNumbers.cpp
+++ simpleLibrary/src/addingNumbers.cpp
@@ -1,11 +1,21 @@
-/*
- * Adding unknown number of input 
- * 
- * /
+//
+// Adding unknown number of input 
+// 
+
  
  #include <iostream>
 
  int main (int argc, char *argv[])
  {
+     int sum = 0;
+     int number = 0;
+
+     while (std::cin >> number)
+     {
+         sum += number;
+     }
+
+     std::cout << "Sum of input numbers is : " << sum << std::endl;
+     
      return 0;
  }
