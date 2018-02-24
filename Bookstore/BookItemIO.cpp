0566f922ea6fe6cb60a5aafd868aec4575556f4d
Bert Taeymans <bert.taeymans@tiberium.ch>
2018-02-24 21:22:48 +0100

    Added optimized swap method of variables

--- Bookstore/BookItemIO.cpp
+++ Bookstore/BookItemIO.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include "Sales_item.h"
 
+template <typename T>
+void swap(T& a, T& b)
+{
+  auto aux = std::move(a);
+  a = std::move(b);
+  b = std::move(aux);
+}
+
 int main (int argc, char *argv[])
 {
     Sales_item book;
 
 
-    std::cin >> book; 
+    //std::cin >> book; 
 
-    std::cout << book << std::endl;
+    //std::cout << book << std::endl;
+
+    int a = 3;
+    int b = 8;
+
+    swap (a, b);
+
+    std::cout << "a = " << a << " and b = " << b << std::endl;
 
     return 0;
 }
